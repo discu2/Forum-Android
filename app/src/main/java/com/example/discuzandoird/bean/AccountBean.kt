0fcd09b68bed8e0b005fe9215a7dbb0aa0bd1fbc
asdkmm5050 <asdkmm505@gmail.com>
2022-04-17 23:11:34 +0800

    Add roleIds

--- app/src/main/java/com/example/discuzandoird/bean/AccountBean.kt
+++ app/src/main/java/com/example/discuzandoird/bean/AccountBean.kt
@@ -5,6 +5,7 @@ class AccountBean {
     var username: String? = null
     var email: String? = null
     var nickname: String? = null
+    var roleIds: List<String>? = null
     var auth = AuthBean()
 
 }
