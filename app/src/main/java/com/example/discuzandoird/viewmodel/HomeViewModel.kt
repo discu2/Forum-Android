8488415e08a707086a9e9839ffff242db7001539
asdkmm5050 <asdkmm505@gmail.com>
2022-04-20 03:43:07 +0800

    Add a function for get board list

--- app/src/main/java/com/example/discuzandoird/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/discuzandoird/viewmodel/HomeViewModel.kt
@@ -1,9 +1,44 @@
 package com.example.discuzandoird.viewmodel
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.android.volley.Request
+import com.android.volley.VolleyError
+import com.example.discuzandoird.api.ApiService
+import com.example.discuzandoird.bean.BoardBean
+import com.google.gson.Gson
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel constructor(application: Application) : AndroidViewModel(application) {
 
+    var boardBeanList = MutableLiveData<List<BoardBean>>()
 
+    private val apiService = ApiService(getApplication())
+
+    fun getBoardList(accessToken: String?, e: (volleyError: VolleyError?) -> Unit) {
+
+        apiService.fetchJsonArray(
+            Request.Method.GET,
+            apiService.getBoardList(),
+            null,
+            accessToken,
+            {
+                if (it != null) {
+                    val list: MutableList<BoardBean> = mutableListOf()
+                    for (i in 0 until it.length()) {
+                        val boardBean =
+                            Gson().fromJson(it.get(i)?.toString(), BoardBean::class.java)
+                        list.add(boardBean)
+                    }
+                    boardBeanList.postValue(list)
+                }
+
+            },
+            {
+                e(it)
+            }
+        )
+
+    }
 
 }
